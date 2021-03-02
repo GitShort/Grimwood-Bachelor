43a3706f40b6e0fadef2f28628e446bdd7388b3c
Airidas <airidas12111@gmail.com>
2021-03-02 23:44:24 +0200

    Scene design almost finished / Lighting scenario manager imported

---
+++ Grimwood/Assets/_Main/Scripts/LightManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightManager : MonoBehaviour
+{
+    [SerializeField] Light[] _lightObjects;
+
+    void Start()
+    {
+        foreach (Light light in _lightObjects)
+        {
+            light.enabled = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer.Equals(10))
+        {
+            foreach (Light light in _lightObjects)
+            {
+                light.enabled = true;
+            }
+            Debug.Log("ENTERED");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer.Equals(10))
+        {
+            foreach (Light light in _lightObjects)
+            {
+                light.enabled = false;
+            }
+            Debug.Log("LEFT");
+        }
+    }
+
+}
