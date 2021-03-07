75c9420ac42397789b6d45c8e86a34f67a543a93
Airidas <airidas12111@gmail.com>
2021-03-07 21:54:59 +0200

    Nav mesh surface baked / terrain tree colliders / flashlight toggle mechanics / enemy follow player / enemy animations

---
+++ Grimwood/Assets/_Main/Scripts/EnemyController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] Transform _player;
+    [SerializeField] float _attackDistance = 2f;
+
+    NavMeshAgent _agent;
+    Animator _anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _agent = this.GetComponent<NavMeshAgent>();
+        _anim = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _agent.SetDestination(_player.position);
+        Debug.Log(_agent.remainingDistance);
+        if (_agent.remainingDistance < _attackDistance)
+        {
+            Debug.Log("CloseEnoughToAttack");
+            _anim.SetBool("isAttacking", true);
+        }
+        else
+            _anim.SetBool("isAttacking", false);
+
+        if (_agent.velocity != Vector3.zero)
+        {
+            _anim.SetBool("isRunning", true);
+        }
+        else
+            _anim.SetBool("isRunning", false);
+    }
+}
---
+++ Grimwood/Assets/_Main/Scripts/FlashlightManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class FlashlightManager : MonoBehaviour
+{
+    public SteamVR_Action_Boolean ActionToggle = SteamVR_Input.GetBooleanAction("TriggerToggle");
+
+    private Interactable _interactable;
+
+    [SerializeField] GameObject _lightObjs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _interactable = GetComponent<Interactable>();
+        _lightObjs.SetActive(false);
+    }
+    void Update()
+    {
+        if (_interactable.attachedToHand)
+        {
+            SteamVR_Input_Sources hand = _interactable.attachedToHand.handType;
+
+            if (ActionToggle.GetStateDown(hand))
+            {
+                //Debug.Log("Flashlight toggle " + hand);
+                if (_lightObjs.activeInHierarchy)
+                    _lightObjs.SetActive(false);
+                else
+                    _lightObjs.SetActive(true);
+            }
+        }
+    }
+}
