f9e71342947870f7475358b77aa7ef4ce29fcec5
Airidas <airidas12111@gmail.com>
2021-03-08 23:31:55 +0200

    light switches

--- Grimwood/Assets/_Main/Scripts/LightManager.cs
+++ Grimwood/Assets/_Main/Scripts/LightManager.cs
@@ -1,41 +1,80 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR.InteractionSystem;
 
 public class LightManager : MonoBehaviour
 {
     [SerializeField] Light[] _lightObjects;
+    [SerializeField] GameObject _volumetricBeam;
+
+    [SerializeField] HoverButton hoverButton;
+    [SerializeField] MeshRenderer _rend;
+
+    bool isLightOn = false;
 
     void Start()
     {
-        foreach (Light light in _lightObjects)
-        {
-            light.enabled = false;
-        }
+        TurnOffLight();
+        hoverButton.onButtonDown.AddListener(OnButtonDown);
+    }
+
+    void OnButtonDown(Hand hand)
+    {
+        if (!isLightOn)
+            TurnOnLight();
+        else
+            TurnOffLight();
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.layer.Equals(10))
+        if (other.gameObject.layer.Equals(10) && isLightOn)
         {
-            foreach (Light light in _lightObjects)
-            {
-                light.enabled = true;
-            }
-            Debug.Log("ENTERED");
+            TurnOnLight();
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.layer.Equals(10))
+        if (other.gameObject.layer.Equals(10) && !isLightOn)
+        {
+            TurnOffLight();
+            Debug.Log("Light out of range and turned off");
+        }
+        else if (other.gameObject.layer.Equals(10) && isLightOn)
         {
             foreach (Light light in _lightObjects)
             {
                 light.enabled = false;
             }
-            Debug.Log("LEFT");
+            _volumetricBeam.SetActive(true);
+            _rend.material.EnableKeyword("_EMISSION");
+            Debug.Log("Light out of range and turned on");
         }
+    }
+    void TurnOnLight()
+    {
+        isLightOn = true;
+        foreach (Light light in _lightObjects)
+        {
+            light.enabled = true;
+        }
+        _volumetricBeam.SetActive(true);
+        _rend.material.EnableKeyword("_EMISSION");
+        Debug.Log("Turned on the light");
+    }
+
+    void TurnOffLight()
+    {
+        isLightOn = false;
+        foreach (Light light in _lightObjects)
+        {
+            light.enabled = false;
+        }
+        _volumetricBeam.SetActive(false);
+        _rend.material.DisableKeyword("_EMISSION");
+        Debug.Log("Turned off the light");
     }
 
 }
---
+++ Grimwood/Assets/_Main/Scripts/LightSwitch.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve;
+using Valve.VR.InteractionSystem;
+
+public class LightSwitch : MonoBehaviour
+{
+    public void OnButtonDown(Hand fromHand)
+    {
+        fromHand.TriggerHapticPulse(500);
+    }
+
+    public void OnButtonUp(Hand fromHand)
+    {
+        
+    }
+
+
+}
